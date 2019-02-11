0fdb87f46bd82f63e85c762f273f42ae6b262e8b
Mani Kumar <mk0872191152@gmail.com>
2019-02-12 02:39:38 +0530

    Check if json request is working or not

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.newsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.newsapp.utils.AppController;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=a5fbb8ec5b1e42a0a33d126bb633a736";
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.tv_response);
+
+        makeJsonRequest();
+    }
+
+
+    // fetch data from endpoint
+    private void makeJsonRequest() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(" ", "onResponse: " + response);
+                        textView.setText("Success : " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("Error : " + error);
+            }
+        });
+        AppController.getInstance().addToRequestQueue(jsonObjectRequest);
     }
 }
